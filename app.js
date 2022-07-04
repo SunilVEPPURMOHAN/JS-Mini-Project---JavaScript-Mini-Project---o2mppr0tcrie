c7dbf4c7db548d15a763b6b30b0fc5e596774922
SUNILVEPPURMOHAN <sunilvm2017@email.iimcal.ac.in>
2022-07-04 16:53:22 +0530

    Authentication files

---
+++ app.js
@@ -0,0 +1,23 @@
+//Rules button
+function Show(id) {
+    if (document.getElementById(id).style.display == 'none') {
+        document.getElementById(id).style.display = 'block';
+    }
+    else {
+        document.getElementById(id).style.display = 'none';
+    }
+};
+
+//Submit Button
+function Verify() {
+    var userRef = "sunilvm";
+    var passRef = "nothingness";
+
+    var user = document.getElementById("username").value;
+    var pass = document.getElementById("password").value;
+    if (user == userRef || pass == passRef) {
+        alert("Connected")
+    } else {
+        alert("Now who are we trying to fool here...")
+    }
+};
